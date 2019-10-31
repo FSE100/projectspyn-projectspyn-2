d9d9e33cba49b2054364256bbb32dfeabd523844
Uriel Lopez <namelessuriel@gmail.com>
2019-10-31 11:51:38 -0700

    Updated color code to make sure color sensor is set to mode 2 for accurate reading and distance script that moves car until certain distance is reached now functions.

--- test7.m
+++ test7.m
@@ -1,5 +1,14 @@
-distance = brick.UltrasonicDist(4);
+distance = brick.UltrasonicDist(3);
+brick.SetColorMode(1, 2);
+color = brick.ColorCode(1);
 
-while 1
-    brick.MoveMotorAngleRel('A', 20, -1440, 'Brake');
+while distance >= 25 % if the distance is at or above 51 units, we run the statement above
+    
+    distance = brick.UltrasonicDist(3);
+    color = brick.ColorCode(1);
+    brick.MoveMotor('BC', 40);
+    
+    if distance < 25
+        brick.MoveMotor('BC', 0);
+    end
 end
