4f63038fa6b3c56c3795a1967354b7f16afd712c
Uriel Lopez <namelessuriel@gmail.com>
2019-11-07 11:07:32 -0700

    Added turning to the ultrasonic reader and color is now functioning for red, green, blue, and yellow.

--- brickColor.m
+++ brickColor.m
@@ -4,4 +4,4 @@ color = brick.ColorCode(1);  % Get Color on port 1.
 
 %print color of object
 
-display(color); 
+display(color);
--- test7.m
+++ test7.m
@@ -1,14 +1,33 @@
 distance = brick.UltrasonicDist(3);
-brick.SetColorMode(1, 2);
-color = brick.ColorCode(1);
 
-while distance >= 25 % if the distance is at or above 51 units, we run the statement above
+while distance >= 28 % if the distance is at or above 28 units, we run the statement below
     
     distance = brick.UltrasonicDist(3);
-    color = brick.ColorCode(1);
-    brick.MoveMotor('BC', 40);
     
-    if distance < 25
-        brick.MoveMotor('BC', 0);
+    brick.MoveMotor('B', 40);
+    brick.MoveMotor('C', 43);
+    
+    if distance < 28 % if the distance below 28 units, we run the statement below
+        
+        brick.MoveMotor('BC', 0); 
+        brick.MoveMotorAngleRel('C', 40, -270, 'Brake');
+
+        distance = brick.UltrasonicDist(3);
+        
+        while distance >= 15 % if the distance is at or above 15 units, we run the statement below
+            
+            distance = brick.UltrasonicDist(3);
+            
+            brick.MoveMotor('B', 40);
+            brick.MoveMotor('C', 43);
+            
+            if distance < 15 % if the distance is below 15 units, we run the statement below
+              
+                brick.MoveMotorAngleRel('C', 40, 800, 'Brake');
+                
+            end
+        end
+        
     end
 end
+brick.StopMotor('BC', 'Coast');
